d9c76637f3c334cbef4be0e682792da00e6b0378
whilreal0 <whil.real@gmail.com>
2025-07-30 14:14:41 +0800

    addead new features

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,6 +33,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+            isShrinkResources = false
             isDebuggable = false
             signingConfig = signingConfigs.getByName("debug")
         }
